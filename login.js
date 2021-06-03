1db7de5c640b97e6187c5574f1c2d7d5f2a95a10
yywon <57125912+hyywon@users.noreply.github.com>
2021-06-03 17:13:52 +0900

    Update login.js

--- login.js
+++ login.js
@@ -18,7 +18,7 @@ function logincheck(){
 
     $.ajax({
         type : "POST",                               
-        url : "http://172.30.1.40:8000/account/signup/",                          
+        url : "http://127.0.0.1:8000/account/signup/",                          
         contentType : 'application/json',            
         data : JSON.stringify(loginData),  
         timeout: 30000,
